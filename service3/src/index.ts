998e957ac15eb6995c8953d8cf017278048a4a29
Chris Lim <cli333@protonmail.com>
2020-05-11 13:32:00 -0700

    fixed yml file

--- service3/src/index.ts
+++ service3/src/index.ts
@@ -1,19 +1,13 @@
 import express from "express";
-import fs from "fs";
 
 const app = express();
 
 app.use(express.json());
 
-const rawData = fs.readFileSync("../env.json");
-const { type: serviceType } = JSON.parse(rawData.toString()) as {
-  type: string;
-};
-
 app.get("/api/", (req: express.Request, res: express.Response) => {
-  res.json(`Requesting from ${serviceType}`);
+  res.json("Requesting from Service3");
 });
 
 app.listen(5000, () =>
-  console.log(`Service: ${serviceType} is listening on port 5000`)
+  console.log("Service: Service3 is listening on port 5000")
 );
